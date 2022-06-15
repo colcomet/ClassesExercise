f375092c962c386268a091e56b476895e8fc29b1
Andrew Barber <drewbarber72@gmail.com>
2022-06-14 23:52:05 -0400

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Camry";
+            myCar.Year = 2021;
+
+            Console.WriteLine($"Make: {myCar.Make}");
+            Console.WriteLine($"Model: {myCar.Model}");
+            Console.WriteLine($"Year: {myCar.Year}");
+            
+        }
+    }
+}
